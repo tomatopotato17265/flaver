6895248394b26e4217b9d6f1fd16cb8d211c4994
tomato potato <nimaigos108@gmail.com>
2026-09-11 20:34:55 -0700

    feat(ui): update friends screen layout

--- src/client/java/tomatopotato/flaver/client/gui/FriendsScreen.java
+++ src/client/java/tomatopotato/flaver/client/gui/FriendsScreen.java
@@ -1,13 +1,14 @@
 package tomatopotato.flaver.client.gui;
 
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.FocusableTextWidget;
+import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 
 public class FriendsScreen extends Screen {
 	private final Screen parent;
+	private final HeaderAndFooterLayout layout = new HeaderAndFooterLayout(this);
 
 	public FriendsScreen(Screen parent) {
 		super(Component.literal("Friends"));
@@ -16,15 +17,17 @@ public class FriendsScreen extends Screen {
 
 	@Override
 	protected void init() {
-		FocusableTextWidget textWidget = this.addRenderableWidget(
-				FocusableTextWidget.builder(this.title, this.font, 12)
-						.textWidth(this.font.width(this.title))
-						.build());
-		textWidget.setPosition(this.width / 2 - textWidget.getWidth() / 2, this.height / 4);
+		this.layout.addTitleHeader(this.title, this.font);
 
-		this.addRenderableWidget(Button.builder(CommonComponents.GUI_DONE, button -> this.onClose())
-				.bounds(this.width / 2 - 100, this.height - 40, 200, 20)
-				.build());
+		this.layout.addToFooter(Button.builder(CommonComponents.GUI_DONE, button -> this.onClose()).build());
+
+		this.layout.visitWidgets(this::addRenderableWidget);
+		this.repositionElements();
+	}
+
+	@Override
+	protected void repositionElements() {
+		this.layout.arrangeElements();
 	}
 
 	@Override
